33a2bd4c07dc819da624bea819057f97ea181f9b
Stoaser <jerome.haluska@gmx.de>
2016-10-23 16:01:20 +0200

    Added a test scene for gradients

---
+++ TextGame/Classes/Scenes/GradientScene.cs
@@ -0,0 +1,41 @@
+﻿namespace Game.Scenes
+{
+    using OpenTK.Graphics;
+    using System;
+    using System.Collections.Generic;
+    using Utility;
+
+    public class GradientScene : Scene
+    {
+        private List<ColoredString> _colStringList = new List<ColoredString>();
+
+        public GradientScene(ExtendedConsoleWindow console) : base(console)
+        {
+            var rnd = new Random();
+
+            for (int cnt = 0; cnt < Console.Rows; cnt++) {
+                var colString = new ColoredString("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod ", Colors.TextColor, Colors.BackgroundColor);
+
+                colString.AddGradient(
+                    new Color4((float)rnd.NextDouble(), (float)rnd.NextDouble(), (float)rnd.NextDouble(), 1),
+                    new Color4((float)rnd.NextDouble(), (float)rnd.NextDouble(), (float)rnd.NextDouble(), 1),
+                    true,
+                    false);
+                colString.AddGradient(
+                    new Color4((float)rnd.NextDouble(), (float)rnd.NextDouble(), (float)rnd.NextDouble(), 1),
+                    new Color4((float)rnd.NextDouble(), (float)rnd.NextDouble(), (float)rnd.NextDouble(), 1),
+                    false,
+                    true);
+                _colStringList.Add(colString);
+            }
+        }
+
+        public override void Draw()
+        {
+            var cnt = 0;
+            foreach (ColoredString colString in _colStringList) {
+                Console.Write(cnt++, 0, colString);
+            }
+        }
+    }
+}
