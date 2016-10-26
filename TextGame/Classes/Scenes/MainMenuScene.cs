a1248d8e28236b3326e6f3503f889724fc6a4e02
Stoaser <jerome.haluska@gmx.de>
2016-10-26 22:47:54 +0200

    Added a dialogue box that displays a continous text rainbow!

--- TextGame/Classes/Scenes/MainMenuScene.cs
+++ TextGame/Classes/Scenes/MainMenuScene.cs
@@ -2,6 +2,7 @@
 {
     using Controls;
     using OpenTK.Graphics;
+    using Utility;
 
     /// <summary>
     /// The main menu of the game.
@@ -39,10 +40,24 @@
             Add(button3);
             Add(button4);
 
-            // Dev Test: DialogueBox
-            DialogueBox.Delay = 5;
-            var dBox = new DialogueBox(this, 18, 1, 61, 23, Colors.TextColor);
-            dBox.WriteSlow("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. At vero eos et accusam et justo duo dolores et ea rebum.Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.At vero eos et accusam et justo duo dolores et ea rebum.Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum.sanctus sea sed takimata ut vero voluptua.est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur");
+            // Rainbow Text with Dialogue Box.
+            DialogueBox.Delay = 1;
+            var dBox = new DialogueBox(this, 18, 1, 61, 23);
+            var colString = new ColoredString("");
+            var gradientColString = new ColoredString("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
+            for (int cnt = 0; cnt < 10; cnt++) {
+                gradientColString.AddGradient(Color4.Yellow, Color4.Green, true);
+                colString += gradientColString;
+                gradientColString.AddGradient(Color4.Green, Color4.Blue, true);
+                colString += gradientColString;
+                gradientColString.AddGradient(Color4.Blue, Color4.Violet, true);
+                colString += gradientColString;
+                gradientColString.AddGradient(Color4.Violet, Color4.Red, true);
+                colString += gradientColString;
+                gradientColString.AddGradient(Color4.Red, Color4.Yellow, true);
+                colString += gradientColString;
+            }
+            dBox.Write(colString);
             Add(dBox);
         }
         
