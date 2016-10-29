1b298762d3fae2aa53c5794feac13a0d06772ee5
Stoaser <jerome.haluska@gmx.de>
2016-10-29 19:42:43 +0200

    Fixed background color

--- TextGame/Classes/Controls/Elements/DefaultAppearance/DefaultTextBox.cs
+++ TextGame/Classes/Controls/Elements/DefaultAppearance/DefaultTextBox.cs
@@ -5,7 +5,7 @@
     public static class DefaultTextBox
     {
         public static Color4 TextColor { get; set; } = Colors.TextColor;
-        public static Color4 BackgroundColor { get; set; } = Colors.SecondaryColor;
+        public static Color4 BackgroundColor { get; set; } = Colors.Darken(Colors.BackgroundColor);
         public static Color4 HeaderTextColor { get; set; } = Colors.HeaderColor;
         public static Color4 HeaderBackgroundColor { get; set; } = Colors.Darken(BackgroundColor);
     }
