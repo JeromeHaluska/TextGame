86dae58de11eac16324f77b8415a50ae2ecac0fe
Stoaser <jerome.haluska@gmx.de>
2016-11-05 14:35:37 +0100

    Now derives from Entity

--- TextGame/Classes/Entities/Player.cs
+++ TextGame/Classes/Entities/Player.cs
@@ -1,7 +1,22 @@
 ﻿namespace Game.Entities
 {
-    public class Player
+    public class Player : Entity
     {
-
+        public Player(
+            string name,
+            int damage, float damageMultiplier,
+            int armor, float armorMultiplier,
+            float hitChance, float hitChanceMultiplier,
+            float criticalHitChance, float criticalHitChanceMultiplier,
+            int criticalDamage, float criticalDamageMultiplier)
+            : base(
+            name,
+            damage, damageMultiplier,
+            armor, armorMultiplier,
+            hitChance, hitChanceMultiplier,
+            criticalHitChance, criticalHitChanceMultiplier,
+            criticalDamage, criticalDamageMultiplier)
+        {
+        }
     }
 }
