dc2fbd06f4fe5fe02fa330122d2717e8abc56ecf
Stoaser <jerome.haluska@gmx.de>
2016-11-19 15:34:51 +0100

    Added method to convert item into a colored string

--- TextGame/Classes/Items/Item.cs
+++ TextGame/Classes/Items/Item.cs
@@ -1,5 +1,8 @@
 ﻿namespace Game.Items
 {
+    using Draw.Utility;
+    using OpenTK.Graphics;
+
     public enum ItemType
     {
         Other,
@@ -22,15 +25,31 @@
 
     public class Item
     {
+        public Item(string name, ItemType type, ItemRarity rarity)
+        {
+            Name = name;
+            Type = type;
+            Rarity = rarity;
+        }
+
+        public Color4[] ItemRarityColors = new Color4[] {
+            new Color4(1, 1, 1, 1),
+            new Color4(0.12f, 1, 0, 1),
+            new Color4(0, 0.44f, 1, 1),
+            new Color4(0.64f, 0.21f, 0.93f, 1),
+            new Color4(1, 0.5f, 0, 1),
+            new Color4(0.9f, 0.8f, 0.5f, 1)
+        };
+
         public string Name;
 
         public ItemType Type { get; set; }
 
         public ItemRarity Rarity { get; set; }
 
-        public Item(string name, ItemType type, ItemRarity rarity)
+        public ColoredString ToColoredString()
         {
-
+            return new ColoredString(Name, ItemRarityColors[(int)Rarity]);
         }
     }
 }
