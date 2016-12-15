6f11e63306f40e7d2e4d215b3f845b18f59feb6b
Stoaser <jerome.haluska@gmx.de>
2016-12-16 00:19:08 +0100

    Added status effect class

---
+++ TextGame/Classes/Combat/StatusEffects/StatusEffect.cs
@@ -0,0 +1,23 @@
+﻿namespace Game.StatusEffects
+{
+    using System.Collections.Generic;
+    using Conditions;
+    using Entities;
+
+    public abstract class StatusEffect
+    {
+        private List<Entity> _linkedEntities = new List<Entity>();
+
+        public StatusEffect(ICondition condition)
+        {
+            Condition = condition;
+        }
+
+        public ICondition Condition { get; private set; }
+
+        public virtual void Update()
+        {
+
+        }
+    }
+}
