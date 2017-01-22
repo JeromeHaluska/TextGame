19e8fbc7da923557aa483b8f1fe7325a7ca7a128
Stoaser <jerome.haluska@gmx.de>
2017-01-22 17:12:36 +0100

    Initial commit

--- TextGame/Classes/Skills/Skill.cs
+++ TextGame/Classes/Skills/Skill.cs
@@ -1,7 +1,18 @@
 ﻿namespace Game.Skills
 {
+    using System;
+
     public abstract class Skill
     {
+        public static Skill GetSkillInstance(string skillName)
+        {
+            // Get the type of the skill.
+            var skillType = Type.GetType("Game.Skills." + skillName);
 
+            // Create a new instance of the skill
+            var skillInstance = (Skill)Activator.CreateInstance(skillType);
+
+            return skillInstance;
+        }
     }
 }
